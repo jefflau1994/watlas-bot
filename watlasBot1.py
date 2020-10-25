c111fa9e69a8498f4103e30a4463b154cfd9d21e
jefflau1994 <47215127+jefflau1994@users.noreply.github.com>
2020-10-25 13:16:19 +0800

    Update watlasBot1.py

--- watlasBot1.py
+++ watlasBot1.py
@@ -35,13 +35,14 @@ def bot1():
     element = browser.find_element_by_xpath('/html/body/center/form/input[4]')
     element.click()
     
-    #讀取庫存 & 時間
+    #READ inventory & game time
     browser.get('http://watlas.fantasyarea.com/watlas/action.cgi')
     
     breadCount = int(wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/center/table[3]/tbody/tr[2]/td[6]'))).text[:-1])
     wineCount = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[3]/td[6]').text[:-1])
     eggCount = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[4]/td[6]').text[:-1])
     ship1count = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[5]/td[6]').text[:-1])
+    ship2count = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[6]/td[6]').text[:-1])
     stamina = browser.find_element_by_xpath("/html/body/center/form/table/tbody/tr[1]/td[4]/table/tbody/tr[3]/td[2]").text
     
     if ("分" in stamina):
@@ -59,11 +60,11 @@ def bot1():
     minute = float(minute)/60
     gameTime = hour + minute
     
-    #print to console
+    #ACTION print to console
     localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print(localtime+"\t"+"bread:"+str(breadCount)+"/1000\t"+"wine:"+str(wineCount)+"/500\t"+"egg:"+str(eggCount)+"/1600\t"+"ship1:"+str(ship1count)+"/50\t"+"Game Time:"+str(int(gameTime))+"h")
+    print(localtime+"\t包:"+str(breadCount)+"\t酒:"+str(wineCount)+"\t蛋:"+str(eggCount)+"\t船1:"+str(ship1count)+"\t船2:"str(ship2count)+"\t時間:"+str(int(gameTime))+"h")
     
-    #補充庫存(麵包 & 酒 & 蛋 & 船)
+    #ACTION re-stock inventory (bread & wine & egg & ship 1/2)
     if breadCount == 0:
         browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=buy&buy=0!0!18&bk=m!&')
         element = browser.find_element_by_xpath('/html/body/center/form/input[7]')
@@ -91,7 +92,7 @@ def bot1():
         gameTime -=0.217
         print("egg purchased")
 
-    if (ship1count == 0 and gameTime > 33.8): 
+    if (ship1count == 0 and gameTime > 11.3): 
         browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=buy&buy=0!3!15&bk=m!&')
         element = browser.find_element_by_xpath('/html/body/center/form/input[7]')
         element.send_keys("50")
@@ -107,10 +108,41 @@ def bot1():
         element.send_keys("50")
         element = browser.find_element_by_xpath('/html/body/center/form/input[6]')
         element.click()
-        gameTime -=33.8
+        gameTime -=11.3
         print("ship 1 manufactured")
 
-    #打掃
+    if (ship2count == 0 and gameTime > 8.77): 
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=buy&buy=0!3!15&bk=m!&')
+        element = browser.find_element_by_xpath('/html/body/center/form/input[7]')
+        element.send_keys("25")
+        element = browser.find_element_by_xpath('/html/body/center/form/input[8]')
+        element.click()
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=buy&buy=0!4!16&bk=m!&')
+        element = browser.find_element_by_xpath('/html/body/center/form/input[7]')
+        element.send_keys("25")
+        element = browser.find_element_by_xpath('/html/body/center/form/input[8]')
+        element.click()
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=item-m&item=1&no=1&&bk=s')
+        element = browser.find_element_by_xpath('/html/body/center/form/input[5]')
+        element.send_keys("25")
+        element = browser.find_element_by_xpath('/html/body/center/form/input[6]')
+        element.click()
+        gameTime -=8.77
+        print("ship 2 manufactured")
+
+    #ACTION buy seed
+    browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=manor')
+    seed1count = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[2]/td[4]').text[0])
+    seed2count = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[3]/td[4]').text[0])
+    seed3count = int(browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr[4]/td[4]').text[0])
+    if seed1count == 0:
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=manor-m&&buy=0&bk=manor')
+    if seed2count == 0:
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=manor-m&&buy=1&bk=manor')
+    if seed3count == 0:
+        browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=manor-m&&buy=2&bk=manor')
+
+    #ACTION sweep the store
     browser.get('http://watlas.fantasyarea.com/watlas/action.cgi?key=sweep')
     rubbish_text = browser.find_element_by_xpath('/html/body/center/table[3]/tbody/tr/td[2]').text
     num = re.findall(r"\d+", rubbish_text)[1]
@@ -135,8 +167,3 @@ def bot1():
 while (1):
     bot1()
     time.sleep(900)
-
-
-
-
-
